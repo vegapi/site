a5b9c1cfdc6a1362745461d89386e145ce45eb21
psvieira <pedro.vieira@mac.com>
2015-06-08 13:10:26 +0100

    Moved generated html files to ./docs

--- docs/upload-docs.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-aws s3 cp api-guide.html s3://vegapi.org/
-aws s3 cp index.html s3://vegapi.org/
--- upload-files.sh
+++ upload-files.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-aws s3 cp api-guide.html s3://vegapi.org/
+aws s3 cp ./docs/api-guide.html s3://vegapi.org/
 aws s3 cp index.html s3://vegapi.org/
